b1253119d935d6acf2603861d6f73b087fc1f2ce
Matheusdnf <matheusdiniz870@gmail.com>
2025-04-09 20:04:49 -0300

    feat - questão 17

---
+++ tarefas/t01/querys/tarefa01-q017.sql
@@ -0,0 +1,16 @@
+create view gerentes as
+	select f.nome,f.codigo,f.salario,d.codigo as cod_dep from funcionario f
+join departamento d on f.codigo=d.cod_gerente;
+
+create view gerente_mais_rico as
+	select g.nome,g.codigo,g.cod_dep from gerentes g
+where g.salario = (select max(salario) from gerentes);
+
+
+select p.codigo,p.descricao from projeto p
+join gerente_mais_rico gmr on p.cod_depto = gmr.cod_dep;
+
+
+
+
+
