6faae7473edff51c2aabb3279e7b03df17f58da2
Matheusdnf <matheusdiniz870@gmail.com>
2025-04-09 21:44:34 -0300

    feat - query 04 #1

--- tarefas/t01/querys/tarefa01-q04.sql
+++ tarefas/t01/querys/tarefa01-q04.sql
@@ -2,7 +2,7 @@ create view todos_gerentes as
 SELECT d.cod_gerente
     FROM departamento d
     WHERE d.cod_gerente IS NOT NULL;
-    
+
 SELECT f.nome, f.salario,f.codigo
 FROM funcionario f
 JOIN departamento d ON f.cod_depto = d.codigo
