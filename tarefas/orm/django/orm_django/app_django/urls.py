f86dce81af1f94ad08e3faf30fae39cf6e0b9854
Matheusdnf <matheusdiniz870@gmail.com>
2025-05-20 16:26:58 -0300

    feat - rotas django #2

--- tarefas/orm/django/orm_django/app_django/urls.py
+++ tarefas/orm/django/orm_django/app_django/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('atividade/adicionar/', views.adicionar_atividade, name='adicionar_atividade'),
     path('projeto/atualizar-lider/', views.atualizar_lider, name='atualizar_lider'),
     path('projetos/', views.listar_projetos, name='listar_projetos'),
+    path('', views.home, name="home"),
 ]
+
